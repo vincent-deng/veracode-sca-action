4b848bc53a3d3f3c46098e70b789a598c2fac0c4
Vincent Deng <vincentdeng.it@gmail.com>
2023-01-30 06:39:12 +1100

    add atifact #4

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ const artifact = require('@actions/artifact');
 
 async function run() {
   let failJob = false;
+  let output;
   try {
-    execSync(`curl -sSL https://download.sourceclear.com/ci.sh | sh -s scan --json='srcclr-output.json' > srcclr-output.txt`)
+    output = execSync(`curl -sSL https://download.sourceclear.com/ci.sh | sh -s -- scan --json='srcclr-output.json'`)
   } catch(error) {
     failJob = true;
   }
 
   const artifactClient = artifact.create()
   const artifactName = 'Veracode SCA Results';
-  const files = ['srcclr-output.txt', 'srcclr-output.json'];
+  const files = [
+    // 'srcclr-output.txt', 
+    'srcclr-output.json'
+  ];
   const rootDirectory = process.cwd()
   const options = { continueOnError: true }
   await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options);
 
-
-  const output = execSync('cat srcclr-output.txt').toString();
   if (failJob) setFailed(output)
 }
 
