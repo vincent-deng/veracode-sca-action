51d3a2ba5d2449c839bd94823585407986c58a0c
Vincent Deng <vincentdeng.it@gmail.com>
2023-01-29 14:50:49 +1100

    test execsync 2

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const { execSync } = require('child_process');
 
 async function run() {
-  const output = execSync('ls -l').toString();
+  try {
+    execSync('curl -sSL https://download.sourceclear.com/ci.sh | sh -s scan > srcclr-output.txt')
+  } catch(error) {
+    console.log(error);
+  }
+
+  const output = execSync('cat srcclr-output.txt').toString();
   console.log(output);
 }
 
