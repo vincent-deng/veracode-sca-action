ec8b5d84ae4acf442c2b76678711fb4c26c1987c
Vincent Deng <vincentdeng.it@gmail.com>
2023-01-30 05:41:56 +1100

    add fail state

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const { execSync } = require('child_process');
+const { setFailed } = require('@actions/core');
 
 async function run() {
+  let failJob = false;
   try {
     execSync('curl -sSL https://download.sourceclear.com/ci.sh | sh -s scan > srcclr-output.txt')
   } catch(error) {
-    console.log(error);
+    failJob = true;
   }
 
   const output = execSync('cat srcclr-output.txt').toString();
   console.log(output);
+
+  if (failJob) core.setFailed(output)
 }
 
 run();
